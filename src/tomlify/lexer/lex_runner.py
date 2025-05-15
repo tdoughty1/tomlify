f5aafc990dc9cbbba282d8ec7332458b736bdeb0
Todd Doughty <w.todd.doughty@gmail.com>
2025-05-15 06:51:10 -0700

    Refactor to have a lex runner and a parse runner

--- src/tomlify/lexer/lex_runner.py
+++ src/tomlify/lexer/lex_runner.py
@@ -7,27 +7,19 @@ from tomlify.lexer.lexer import Lexer
 from tomlify.parser.parser import Parser
 
 
-def run(source: str, lex_only: bool) -> None:
+def run(source: str) -> None:
     lexer = Lexer(source)
     lexer.lex()
     tokens = lexer.get_tokens()
-    if lex_only:
-        for token in tokens:
-            typer.echo(f"{token}")
-    if not lex_only:
-        parser = Parser(tokens)
-        expressions = parser.parse()
-        for expression in expressions:
-            typer.echo(expression)
+    for token in tokens:
+        typer.echo(f"{token}")
 
-def run_file(path: Path, lex_only: bool) -> None:
+def run_file(path: Path) -> None:
     with Path.open(path) as f:
-        run(f.read(), lex_only)
+        run(f.read())
 
-lex_option = typer.Option(False, "--lex", help="Only Run Lexer on input file")
-
-def main(path: str, lex_only: bool = lex_option) -> None:
-    run_file(Path(path), lex_only)
+def main(path: str) -> None:
+    run_file(Path(path))
 
 
 if __name__ == "__main__":
--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -7,7 +7,7 @@ RESOURCE_PATH = "tests/resources"
 RUNNER_FILE = "src/tomlify/lexer/lex_runner.py"
 
 def run_test(test_file: Path) -> tuple[str, str, int]:
-    command = ["uv", "run", RUNNER_FILE, "--lex", str(test_file)]
+    command = ["uv", "run", RUNNER_FILE, str(test_file)]
     process = Popen(command, stdout=PIPE, stderr=PIPE, text=True)
     out, err = process.communicate()
     code = process.returncode
